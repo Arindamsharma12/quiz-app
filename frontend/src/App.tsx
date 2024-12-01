e31677b98d5acdd05601e5eeda28f1370911c210
Arindamsharma12 <117537886+Arindamsharma12@users.noreply.github.com>
2024-12-01 22:57:55 +0530

    commint now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Admin } from './components/Admin'
 import { User } from './components/User'
 import './App.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { HashRouter,Routes,Route } from 'react-router-dom'
 
 
 function App() {
@@ -9,12 +9,12 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
             <Route path='/admin' element={<Admin/>}/>
             <Route path='/user' element={<User/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
